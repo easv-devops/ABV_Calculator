bd94f016a66fde342678a215105739845a87e010
chrsut01 <chrissu360@gmail.com>
2024-05-17 10:47:18 +0200

    unit tests

--- ABV_Calculator/obj/Debug/net8.0/ABV_Calculator.AssemblyInfo.cs
+++ ABV_Calculator/obj/Debug/net8.0/ABV_Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABV_Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfda53dcbffd98e7f8746d3753c6baae8e8ad93c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb0e2663fb2e89eae2a096726a6db7c7ab5f1fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABV_Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABV_Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
