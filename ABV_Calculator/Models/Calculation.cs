4c2fdccb5a8a9fc5857c31e378b0d12fe4e68355
chrsut01 <chrissu360@gmail.com>
2024-05-19 18:32:16 +0200

    upadted Calculation model

--- ABV_Calculator/Models/Calculation.cs
+++ ABV_Calculator/Models/Calculation.cs
@@ -1,10 +1,19 @@
 namespace ABV_Calculator.Models;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 public class Calculation 
 {
     public int Id { get; set; }
+    [Required]
+    [Column("original_gravity")]  // Map to the original_gravity column in the database
     public double OriginalGravity { get; set; }
+
+    [Required]
+    [Column("final_gravity")]  // Map to the final_gravity column in the database
     public double FinalGravity { get; set; }
+
+    [Required]
+    [Column("abv")]  // Map to the abv column in the database
     public double Abv { get; set; }
-    public DateTime Date { get; set; }
+    public DateTime date_time { get; set; }
 }
