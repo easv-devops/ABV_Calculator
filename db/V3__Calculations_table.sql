3965ef227bc515b7b6343c41308668c226f1605f
chrsut01 <chrissu360@gmail.com>
2024-05-24 12:40:15 +0200

    V3__ Calculations added to create mariadb ABV-Calculations DB

---
+++ db/V3__Calculations_table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Calculations(
+                             Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                             original_gravity DOUBLE NOT NULL,
+                             final_gravity DOUBLE NOT NULL,
+                             abv DOUBLE NOT NULL,
+                             date_time TIMESTAMP NOT NULL)                
+   
