f1b7df3e0e9ba8d354ff348b00f51cec35af5bc0
chrsut01 <chrissu360@gmail.com>
2024-05-26 16:15:57 +0200

    database unit test moved to Tests project

--- Tests/Unit/CalculationRepositoryTests.cs
+++ Tests/Unit/CalculationRepositoryTests.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Moq.EntityFrameworkCore;
 
-namespace ABV_Calculator.Data
+namespace Tests.Unit
 {
     [TestFixture]
     public class CalculationRepositoryTests
