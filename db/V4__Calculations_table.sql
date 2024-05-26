e4cd1cff68434a8d4e4a2910de22387173b7bcd3
chrsut01 <chrissu360@gmail.com>
2024-05-26 11:50:43 +0200

    update docker-compose and V4__

---
+++ db/V4__Calculations_table.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS Calculations;
+
+CREATE TABLE Calculations(
+                             Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                             original_gravity DOUBLE NOT NULL,
+                             final_gravity DOUBLE NOT NULL,
+                             abv DOUBLE NOT NULL,
+                             date_time TIMESTAMP NOT NULL)  
