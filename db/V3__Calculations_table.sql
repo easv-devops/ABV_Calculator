85ea3eea1cace2b5634f1e1be20cc71f20338e4a
chrsut01 <chrissu360@gmail.com>
2024-05-27 13:58:59 +0200

    deleted V3 and V4

--- db/V3__Calculations_table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE Calculations(
-                             Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                             original_gravity DOUBLE NOT NULL,
-                             final_gravity DOUBLE NOT NULL,
-                             abv DOUBLE NOT NULL,
-                             date_time TIMESTAMP NOT NULL)                
-   
