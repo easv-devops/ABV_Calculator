63c620bdc7a41852e8ef3f96cb15b49584cbe14b
chrsut01 <chrissu360@gmail.com>
2024-05-30 09:13:53 +0200

    updated end2nd

---
+++ ABV_Calculator/Testing/e2e/end2end.js
@@ -0,0 +1,24 @@
+﻿import { Selector } from 'testcafe';
+
+//fixture('ABV Calculation').page('http://localhost:8081/ABVCalculator');
+//fixture('ABV Calculation').page('http://5.189.148.90:8081/AbvCalculatorModel');
+fixture('ABV Calculation').page('http://5.189.134.176:8082/AbvCalculatorModel');
+
+
+
+test('Valid input', async t => {
+
+    await t
+        .wait(5000)
+      //  .expect(Selector('#originalGravity').exists).ok({ timeout: 10000 })
+      
+       // Arrange
+        .typeText('#originalGravity', '1.9')
+        .typeText('#finalGravity', '1.8')
+        .wait(2000)
+        // Act
+        .click('.btn.btn-primary')
+        .wait(2000)
+        // Assert
+        .expect(Selector('#resultsSection').innerText).contains("13.13%");
+});
